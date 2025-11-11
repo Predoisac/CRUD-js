cbffb8d19d42207fe3fdbadba23dc77fca95cd06
Pedro <predoisac@gmail.com>
2025-11-11 19:12:04 -0300

    rumadinha 3 linha '~'

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,8 +1,9 @@
 import jwt from "jsonwebtoken"
+import ServiceUser from "../services/user.js"
 
 const JWTsegredo = "aboba"
 
-export default function authMiddleware(req, res, next) {
+export default async function authMiddleware(req, res, next) {
 
     const token = req.headers['authorization']
     console.log(req.headers['authorization'])
@@ -12,9 +13,10 @@ export default function authMiddleware(req, res, next) {
 
         }
         const decoded = jwt.verify(token.split(' ')[1], JWTsegredo)
-        console.log(decoded)
-        token.split('')[1]
 
+        const user = await ServiceUser.FindOne(decoded.id)
+
+        req.headers.user = user
         next()
     } catch (error) {
         res.status(403).send({
